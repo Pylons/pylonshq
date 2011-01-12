348bb4ab3fed9a63030ead46e3f113bcd9dfc084
Blaise Laflamme <blaise@laflamme.org>
2011-01-12 10:45:42 -0500

    Added basic view tests

--- pylonshq/tests.py
+++ pylonshq/tests.py
@@ -1,19 +1,57 @@
 import unittest
 
-from pyramid.config import Configurator
 from pyramid import testing
+from pyramid.request import TemplateContext
 
-class ViewTests(unittest.TestCase):
+class AppViewTests(unittest.TestCase):
     def setUp(self):
-        self.config = Configurator(autocommit=True)
-        self.config.begin()
+        self.config = testing.setUp()
 
     def tearDown(self):
-        self.config.end()
+        testing.tearDown()
+    
+    def _makeOne(self, context, request):
+        from pylonshq.views import AppView
+        return AppView(context, request)
 
-    def test_my_view(self):
-        from pylonshq.views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'pylonshq')
-
+    def test_home(self):
+        context = testing.DummyResource()
+        request = testing.DummyRequest(tmpl_context=TemplateContext)
+        view = self._makeOne(context, request)
+        home = view.home()
+        self.assertEqual(view.ctx.pagename, 'Home')
+    
+    def test_pylons(self):
+        context = testing.DummyResource()
+        request = testing.DummyRequest(tmpl_context=TemplateContext)
+        view = self._makeOne(context, request)
+        home = view.pylons()
+        self.assertEqual(view.ctx.pagename, 'Pylons Project')
+    
+    def test_projects(self):
+        context = testing.DummyResource()
+        request = testing.DummyRequest(tmpl_context=TemplateContext)
+        view = self._makeOne(context, request)
+        home = view.projects()
+        self.assertEqual(view.ctx.pagename, 'Projects')
+    
+    def test_community(self):
+        context = testing.DummyResource()
+        request = testing.DummyRequest(tmpl_context=TemplateContext)
+        view = self._makeOne(context, request)
+        home = view.community()
+        self.assertEqual(view.ctx.pagename, 'Community')
+    
+    def test_tools(self):
+        context = testing.DummyResource()
+        request = testing.DummyRequest(tmpl_context=TemplateContext)
+        view = self._makeOne(context, request)
+        home = view.tools()
+        self.assertEqual(view.ctx.pagename, 'Tools')
+    
+    def test_test(self):
+        context = testing.DummyResource()
+        request = testing.DummyRequest(tmpl_context=TemplateContext)
+        view = self._makeOne(context, request)
+        home = view.test()
+        self.assertEqual(view.ctx.pagename, 'Test')
