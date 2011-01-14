e25d1b3581b9b2477f7391714064a5b0ffa0c94f
Blaise Laflamme <blaise@laflamme.org>
2011-01-13 22:57:07 -0500

    Added pyramid_sqla in dependancies

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 requires = [
     'pyramid',
     'pyramid_beaker',
+    'pyramid_sqla',
     'SQLAlchemy',
     'transaction',
     'repoze.tm2',
