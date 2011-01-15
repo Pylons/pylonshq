a8d16e393a3d0730206cb6fd076ac929bc10b275
Blaise Laflamme <blaise@laflamme.org>
2011-01-15 15:34:01 -0500

    ignoring sqlite db and basic handler tests

--- pylonshq/tests.py
+++ pylonshq/tests.py
@@ -1,5 +1,8 @@
 import unittest
 
+from pyramid import testing
+from pyramid.request import TemplateContext
+
 class MyHandlerTests(unittest.TestCase):
     def setUp(self):
         from pyramid.config import Configurator
@@ -20,12 +23,42 @@ class MyHandlerTests(unittest.TestCase):
     def _makeOne(self, request):
         from pylonshq.handlers import MainHandler
         return MainHandler(request)
-
+    
     def test_index(self):
-        request = DummyRequest()
+        request = testing.DummyRequest(tmpl_context=TemplateContext)
         handler = self._makeOne(request)
-        info = handler.index()
-        self.assertEqual(info['project'], 'sqla')
+        page = handler.index()
+        self.assertEqual(handler.c.pagename, 'Home')
+    
+    #def test_pylons(self):
+    #    request = testing.DummyRequest(tmpl_context=TemplateContext)
+    #    handler = self._makeOne(request)
+    #    page = handler.pylons()
+    #    self.assertEqual(page.c.pagename, 'Pylons Project')
+    
+    #def test_projects(self):
+    #    request = testing.DummyRequest(tmpl_context=TemplateContext)
+    #    handler = self._makeOne(request)
+    #    page = handler.projects()
+    #    self.assertEqual(handler.c.pagename, 'Projects')
+    
+    #def test_community(self):
+    #    request = testing.DummyRequest(tmpl_context=TemplateContext)
+    #    handler = self._makeOne(request)
+    #    page = handler.community()
+    #    self.assertEqual(handler.c.pagename, 'Community')
+    
+    #def test_tools(self):
+    #    request = testing.DummyRequest(tmpl_context=TemplateContext)
+    #    handler = self._makeOne(request)
+    #    page = handler.tools()
+    #    self.assertEqual(handler.c.pagename, 'Tools')
+    
+    def test_test(self):
+        request = testing.DummyRequest(tmpl_context=TemplateContext)
+        handler = self._makeOne(request)
+        page = handler.test()
+        self.assertEqual(handler.c.pagename, 'Test')
 
 class DummyRequest(object):
     pass
