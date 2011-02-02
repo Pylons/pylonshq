3d002aa54af655ca90de54ca3069488d0096ea69
Marcin Lulek <info@webreactor.eu>
2011-02-02 13:58:15 +0100

    jobs stub

---
+++ pylonshq/handlers/jobs.py
@@ -0,0 +1,22 @@
+import logging
+
+import sqlalchemy as sa
+
+from pyramid.httpexceptions import HTTPFound
+from pyramid.url import route_url
+from pyramid import security
+
+from pyramid_handlers import action
+from pyramid_simpleform import Form
+from pyramid_simpleform.renderers import FormRenderer
+
+from pylonshq.handlers.base import BaseHandler as base
+
+log = logging.getLogger(__name__)
+
+        
+class JobsHandler(base):
+    
+    @action(renderer='pylonshq:templates/home/test.mako')
+    def index(self):
+        return {}
