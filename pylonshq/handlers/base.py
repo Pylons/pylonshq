a328606375037cea2acd5cd2c763d35328109c9a
Chris McDonough <chrism@plope.com>
2013-04-02 06:23:48 -0400

    fix https://errormator.com/report/491aa5f7bac549b8916e7660f149110d/view_report

--- pylonshq/handlers/base.py
+++ pylonshq/handlers/base.py
@@ -3,7 +3,6 @@ import logging
 
 from pyramid.view import view_config
 from pyramid.security import authenticated_userid
-from pyramid_handlers import action
 
 log = logging.getLogger(__name__)
 
@@ -13,7 +12,11 @@ class BaseHandler(object):
         self.request = request
         self.logged_in = authenticated_userid(request)
         self.c = self.request.tmpl_context
-        self.c.active_header_nav = self.request.matchdict.get('action') or 'home'
+        active_header_nav = 'home'
+        if request.matchdict is not None:
+             active_header_nav = self.request.matchdict.get(
+                'action', active_header_nav)
+        self.c.active_header_nav = active_header_nav
         self.c.active_footer_nav = ''
         self.c.masthead_logo = 'pylons'
         self.c.pagename = ''
